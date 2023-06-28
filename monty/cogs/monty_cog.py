cd423d63c4ef90177d48dd79b9b6b9f6ffd04eaf
William Burton <13026711+burtonwilliamt@users.noreply.github.com>
2023-06-28 03:07:58 -0400

    Improved UX on urban dictionary

--- monty/cogs/monty_cog.py
+++ monty/cogs/monty_cog.py
@@ -1,9 +1,11 @@
 from collections.abc import Sequence
+from dataclasses import dataclass
 import datetime
 import json
 import http.client
 import logging
 import random
+import re
 from typing import Any
 import urllib.parse
 
@@ -16,6 +18,8 @@ from faker import Faker
 from monty import money_db
 from monty.cogs.text_options import BEG_OPTIONS
 
+linked_terms_pattern = re.compile(r'\[([^]]*)\]')
+
 _log = logging.getLogger(__name__)
 fake_generator = Faker()
 
@@ -23,6 +27,90 @@ fake_generator = Faker()
 def choose_with_distribution(choices: Sequence[Any]) -> Any:
     weights = [len(choices) - i for i in range(len(choices))]
     return random.choices(choices, weights=weights)[0]
+
+
+@dataclass
+class UrbanDefinition:
+    """Class to store the response of an urban dictionary query."""
+    word: str
+    definition: str
+    example: str
+    permalink: str
+
+
+async def fetch_urban_dictionary_definition(
+        term: str) -> UrbanDefinition | None:
+    # Do the network request.
+    conn = http.client.HTTPSConnection('api.urbandictionary.com')
+    encoded_term = urllib.parse.quote(term, safe='')
+    conn.request('GET', f'/v0/define?term={encoded_term}')
+    res = conn.getresponse()
+    data_bytes = res.read()
+    if len(data_bytes) == 0:
+        return None
+    first_result = json.loads(data_bytes.decode('utf-8'))['list'][0]
+    return UrbanDefinition(word=first_result['word'],
+                           definition=first_result['definition'],
+                           example=first_result['example'],
+                           permalink=first_result['permalink'])
+
+
+async def send_urban_dictionary_definition(
+    interaction: discord.Interaction,
+    term: str,
+    previous_terms: tuple[str] = tuple()):
+    """Respond to the interaction with an urban diction definition."""
+
+    definition = await fetch_urban_dictionary_definition(term)
+    if definition is None:
+        await interaction.response.send_message('Failed to get a definition')
+        return
+
+    # Build a UI
+    terms_to_get_here = tuple(list(previous_terms) + [definition.word])
+    e = discord.Embed()
+
+    def clean(text: str):
+        return '\n'.join([
+            l for l in text.replace(r'\r\n', '\n').replace('[', '__').replace(
+                ']', '__').splitlines() if len(l.strip()) > 0
+        ])
+
+    description = [f'# {" > ".join(terms_to_get_here)}']
+    description.extend(
+        [f'### {l}' for l in clean(definition.definition).splitlines()])
+    description.append('\n')
+    description.extend(
+        [f'*{l}*' for l in clean(definition.example).splitlines()])
+    description.append(f'[website]({definition.permalink})')
+    e.description = '\n'.join(description)
+    other_terms = linked_terms_pattern.findall(
+        definition.definition) + linked_terms_pattern.findall(
+            definition.example)
+    view = discord.ui.View()
+    processed_terms = set()
+    for other_term in other_terms:
+        other_term = other_term.lower()
+        if other_term in processed_terms:
+            continue
+        view.add_item(
+            UbanDictionaryButton(term=other_term,
+                                 previous_terms=terms_to_get_here))
+        processed_terms.add(other_term)
+
+    await interaction.response.send_message(embed=e, view=view)
+
+
+class UbanDictionaryButton(discord.ui.Button):
+
+    def __init__(self, term: str, previous_terms: tuple[str]):
+        super().__init__(label=term)
+        self.term = term
+        self.previous_terms = previous_terms
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        await send_urban_dictionary_definition(interaction, self.term,
+                                               self.previous_terms)
 
 
 class MontyCog(discord.ext.commands.Cog):
@@ -108,27 +196,13 @@ class MontyCog(discord.ext.commands.Cog):
         await interaction.response.send_message(message)
 
     @app_commands.command()
-    async def ud(self, interaction: discord.Interaction, term: str):
-        """Fetches a definition from urban dictionary."""
-        conn = http.client.HTTPSConnection('api.urbandictionary.com')
-        encoded_term = urllib.parse.quote(term, safe='')
-        conn.request('GET', f'/v0/define?term={encoded_term}')
-        res = conn.getresponse()
+    async def urban(self, interaction: discord.Interaction, term: str):
+        """Fetches a definition from urban dictionary.
 
-        data_bytes = res.read()
-        if len(data_bytes) == 0:
-            await interaction.response.send_message('Failed to get a definition'
-                                                   )
-            return
-        data = json.loads(data_bytes.decode('utf-8'))
-        the_list = data['list']
-        first_result = the_list[0]
-
-        word = first_result['word']
-        permalink = first_result['permalink']
-        definition = first_result['definition'].replace(r'\r\n', '\n')
-        e = discord.Embed(description=f'[{word}]({permalink})\n\n' + definition)
-        await interaction.response.send_message(embed=e)
+        Args:
+            term: The term to search for.
+        """
+        await send_urban_dictionary_definition(interaction, term)
 
     @app_commands.command()
     async def beg(self, interaction: discord.Interaction):
